e9f5b0f653cfaed5b21b42ec0f15d0e59437125a
Bophelo <mpilo@vivaldi.net>
2018-08-08 00:00:21 +0200

    Install and Setup Vuex

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <!--<div class="hello">
     <h1>{{ msg }}</h1>
     <p>
       For guide and recipes on how to configure / customize this project,<br>
@@ -27,12 +27,21 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+  </div>-->
+  <div>
+    {{ room }}
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'HelloWorld',
+  computed : {
+    ...mapGetters({
+      room: 'getRoom'
+    })
+  },
   props: {
     msg: String
   }
---
+++ src/store/state.js
@@ -0,0 +1,4 @@
+// allows us to enter a room name and have it available through out the course  of the application regardless of page
+export default {
+  room: 'abc'
+}
